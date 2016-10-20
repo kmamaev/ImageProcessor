a02b3cc4e87aecc611d89d515ba11fb508d7a721
Konstantin Mamaev <kostya.plhs@gmail.com>
2016-10-20 15:18:58 +0600

    Implement prototype of main screen.

--- ImageProcessor/ViewControllers/Main/MainVC.m
+++ ImageProcessor/ViewControllers/Main/MainVC.m
@@ -2,13 +2,26 @@
 
 
 @interface MainVC ()
+@property (strong, nonatomic) IBOutletCollection(UIView) NSArray *invisibleViews;
 @end
 
 
 @implementation MainVC
 
+#pragma mark - ViewController's lifecycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self hideInvisibleViews];
+}
+
+#pragma mark - Configuration
+
+- (void)hideInvisibleViews {
+    for (UIView *view in self.invisibleViews) {
+        view.backgroundColor = [UIColor clearColor];
+    }
 }
 
 @end

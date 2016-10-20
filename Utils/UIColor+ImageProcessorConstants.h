76dae759fe8b1f349d21ccc1f0d7e56d32ebf3df
Konstantin Mamaev <kostya.plhs@gmail.com>
2016-10-20 15:50:03 +0600

    Add file with color constanst.

---
+++ Utils/UIColor+ImageProcessorConstants.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+
+@interface UIColor (ImageProcessorConstants)
+
++ (UIColor *)buttonColor;
+
+@end
---
+++ Utils/UIColor+ImageProcessorConstants.m
@@ -0,0 +1,11 @@
+#import "UIColor+ImageProcessorConstants.h"
+#import "UIColor+HexString.h"
+
+
+@implementation UIColor (ImageProcessorConstants)
+
++ (UIColor *)buttonColor {
+    return [UIColor colorWithHexValue:0xED0881];
+}
+
+@end
